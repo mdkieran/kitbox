f382c8f9d314b85cdd07e8a9ac80a67b15415f0f
Matt Kieran <mdkieran@mdkieran.com>
2017-01-02 23:05:03 +0000

    Made slidemenu component

---
+++ slidemenu.js
@@ -0,0 +1,14 @@
+$(function () {
+	$('button', '.kb.slidemenu').on('click', function () {
+		$li = $(this).closest('li');
+		if ($li.hasClass('active')) {
+			$li.removeClass('active').find('li').removeClass('active');
+			$li.find('button').attr('aria-expanded', 'false');
+		} else {
+			$li.addClass('active');
+			$(this).attr('aria-expanded', 'true');
+		}
+	});
+
+	$('li.active', '.kb.slidemenu').children('div').find('button').attr('aria-expanded', 'true');
+});
